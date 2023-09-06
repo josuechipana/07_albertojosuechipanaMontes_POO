43381efeb1588a8b5e3de6192f2a840b63580ad4
albertochipana <141298020+josuechipana@users.noreply.github.com>
2023-09-05 22:01:26 -0500

    Create animales.js

---
+++ animales.js
@@ -0,0 +1,27 @@
+package pkg01_albertoapaza_poo;
+
+import java.util.Scanner;
+
+
+public class Calculadora {
+
+    int a;
+    int b;
+    int totalsuma;
+    int totalresta;
+    int totalmultiplicar;
+    int totaldivision;
+    
+    public void sumar() {
+        totalsuma = a + b;
+        System.out.println(totalsuma);
+    }
+    
+    public void restar() {
+        totalresta = a - b;
+        System.out.println(totalresta);
+    }
+    
+    public void multiplicar() {
+        totalmultiplicar = a * b;
+        System.out.pr
